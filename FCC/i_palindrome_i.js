bb4baa24c3cc7a780058ddb1f81727e7127a8bd4
Andrew Thomas <anthom@gmail.com>
2016-12-19 16:23:22 -0500

    Consolidate regex, redo with recursion
    The old solution where I parsed out an array from the string wasn't working anymore, so I attempted this with a recursive algorithm instead. Also, consolidated 4 lines of regex into one and, obviously, got rid of the str.split commands.

--- FCC/i_palindrome_i.js
+++ FCC/i_palindrome_i.js
@@ -2,29 +2,24 @@
 // Basic Algorithm Challenge: Check for Palindromes
 
 function palindrome(str) {
-  // create a variable to store number of palindrom matches
-  var palMatch = 0;
-  var strArray = [];
-  // modify str to remove any non alpha characters
-  str = str.replace(/\W+/g,""); // remove non alphanumeric
-  str = str.replace(/\d+/g,""); // remove numbers
-  str = str.replace(/[_-]/g, ""); // remove dashes and underscores
-  str = str.toLowerCase(); // modify str to ensure all lowercase characters
+  str = str.toLowerCase().replace(/[^a-z|0-9]/g, ""); // modify str to ensure all lowercase characters
+
   strArray = str.split(''); // expand contents of str to an array
-  while(strArray.length >= 1) {
-    arrayEnd = str.length - 1;
-    // compare first element in array to last element
-    if (strArray[0] == strArray[arrayEnd]) {
-      palMatch = palMatch + 2;
-     }
-    strArray = strArray.pop;
-    strArray = strArray.shift;
-  }
-  console.log(strArray, palMatch);
-  // if both elements match, increment palmatch by 2
-  // delete first and last element, loop back to compare new first and last
+
+  console.log(str);
   
-  return true;
+  if (str.length === 0) {
+	  return true;
+  }
+  
+  if (str[0] !== str[str.length-1]){
+	  return false
+  }
+  
+  else {
+	  return palindrome(str.slice(1,str.length-1));
+  }
+ 
 }
 
-palindrome("eye");
+palindrome("I, man, am regal! A German, am I!");

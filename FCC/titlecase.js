89d4dd0e3be1457149479a69a5b0fa96f205320a
Andrew Thomas <anthom@gmail.com>
2017-04-03 16:09:26 -0400

    Cleaned up code a bit, removed redundant stuff

--- FCC/titlecase.js
+++ FCC/titlecase.js
@@ -2,19 +2,18 @@
 // Basic Algorithm Challenge: Title Case A Sentence
 
 function titleCase(str) {
-  str = str.toLowerCase(); // make all letters lower case
-  words = str.split("");
-  arraySize = words.length;
+  chars = str.toLowerCase().split(""); // make all letters lower case
   // capitalize first letter
-  words[0] = words[0].toUpperCase();
+  chars[0] = chars[0].toUpperCase();
   // capitalize all letters that occur after a space
-  for (i = 0; i < arraySize; i++) {
-    if (words[i-1]===" ") {
-      words[i] = words[i].toUpperCase();
+  for (i = 0; i < chars.length; i++) {
+    if (chars[i-1]===" ") {
+      chars[i] = chars[i].toUpperCase();
     }
-  str = words.join("");
+  str = chars.join("");
   }
   return str;
 }
 
-titleCase("HERE IS MY HANDLE HERE IS MY SPOUT");
+titleCase("I'm a little tea pot");
+

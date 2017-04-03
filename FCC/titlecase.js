702c46e2857f4df0631d424da21bc5c027303ed2
Andrew Thomas <anthom@gmail.com>
2017-04-03 15:58:40 -0400

    Create titlecase.js

---
+++ FCC/titlecase.js
@@ -0,0 +1,21 @@
+// Free Code Camp, Front End track
+// Basic Algorithm Challenge: Title Case a Sentence
+
+function titleCase(str) {
+  str = str.toLowerCase(); // make all letters lower case
+  words = str.split("");
+  arraySize = words.length;
+  console.log(words);
+  // capitalize first letter
+  words[0] = words[0].toUpperCase();
+  // capitalize all letters that occur after a space
+  for (i = 0; i < arraySize; i++) {
+    if (words[i-1]===" ") {
+      words[i] = words[i].toUpperCase();
+    }
+  str = words.join("");
+  }
+  return str;
+}
+
+titleCase("HERE IS MY HANDLE MUTHFUCKA");
